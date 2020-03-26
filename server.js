47f306be7c98d9275ad3a26a149529d5e51ac9da
Bharat <athotabharat@gmail.com>
2020-03-26 22:44:33 +0100

    bug fixes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/FarmConnect'));
+app.use(express.static(__dirname + '/dist/farmconnect'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/FarmConnect/index.html'));
+res.sendFile(path.join(__dirname+'/dist/farmconnect/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
