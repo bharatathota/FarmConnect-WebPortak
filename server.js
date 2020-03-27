60ea184eee9b3a9bd594e211b7ea6db8427b862c
Bharat <athotabharat@gmail.com>
2020-03-27 09:56:27 +0100

    changes in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/farmconnect'));
+app.use(express.static(__dirname + '/farm-connect'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/farmconnect/index.html'));
+res.sendFile(path.join(__dirname+'/farm-connect/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
